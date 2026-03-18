1ab1c46442861048ec27ad9e026d550f9e261024
Scott <20427861+scott-janes@users.noreply.github.com>
2026-03-18 20:53:05 +0000

    Refactor wezterm config install script
    Stop it from deleting everything

--- scripts/configs/wezterm.sh
+++ scripts/configs/wezterm.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# wezterm.sh - WezTerm config install script
 set -e
+
 REPO_DIR="$(cd "$(dirname "$0")"/../.. && pwd)"
 CONFIG_SRC="$REPO_DIR/wezterm/wezterm.lua"
 CONFIG_DEST="$HOME/.wezterm.lua"
+
 source "$REPO_DIR/scripts/core/helpers.sh"
 
 msg "Refreshing wezterm config..."
-ensure_config_dir
-refresh_config_dir "$(dirname "$CONFIG_DEST")"
+
 copy_config "$CONFIG_SRC" "$CONFIG_DEST"
+
 msg "wezterm config refreshed."
